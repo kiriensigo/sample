10f8e571daf7673bab52cc38a53ef51e88f9c71f
kiriensigo <kiriensigo@gmail.com>
2024-07-21 19:29:48 +0900

    11.1

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ENV['TZ'] = 'UTC'
+
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.

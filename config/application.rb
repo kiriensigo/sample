633d5bb6b542b08179e39fa13dae41de05e72c17
kiriensigo <kiriensigo@gmail.com>
2024-08-10 01:33:17 +0900

    15.8

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 ENV['TZ'] = 'UTC'
 
+
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.

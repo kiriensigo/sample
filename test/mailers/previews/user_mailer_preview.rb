3e8f31d578686a3e9bc26ac0f2646a886a21a865
kiriensigo <kiriensigo@gmail.com>
2024-08-28 22:56:18 +0900

    15.74

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -8,7 +8,6 @@ class UserMailerPreview < ActionMailer::Preview
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
-  
 
   # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
